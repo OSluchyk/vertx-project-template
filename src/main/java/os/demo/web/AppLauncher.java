5327ea925f62b846e0850bf2f36386570f35447e
oleksandr <oleksandr_sluchyk@epam.com>
2020-03-08 17:50:44 +0200

    add worker verticle example

--- src/main/java/os/demo/web/AppLauncher.java
+++ src/main/java/os/demo/web/AppLauncher.java
@@ -24,6 +24,20 @@ public class AppLauncher extends Launcher {
         options.setMaxEventLoopExecuteTime(10);
         options.setMaxEventLoopExecuteTimeUnit(TimeUnit.MILLISECONDS);
 
+        // check for blocked threads every 5s
+        options.setBlockedThreadCheckInterval(5);
+        options.setBlockedThreadCheckIntervalUnit(TimeUnit.SECONDS);
+
+        // warn if an worker thread handler took more than 10s to execute
+        options.setMaxWorkerExecuteTime(10);
+        options.setMaxWorkerExecuteTimeUnit(TimeUnit.SECONDS);
+
+        // log the stack trace if an event loop or worker handler took more than 20s to execute
+        options.setWarningExceptionTime(20);
+        options.setWarningExceptionTimeUnit(TimeUnit.SECONDS);
+
+
+
         logger.info("Vertx options: {}", options);
     }
 
@@ -32,8 +46,15 @@ public class AppLauncher extends Launcher {
         int instances = deploymentOptions.getInstances();
         if (instances < 0) {
             instances = VertxOptions.DEFAULT_EVENT_LOOP_POOL_SIZE;
-            deploymentOptions.setInstances(instances);
         }
+
+        if (deploymentOptions.isWorker()) {
+            //Worker verticles handle events on worker threads
+            // but a single instance can only handle one event at a given point in time.
+            // So you shall deploy as many instances as worker threads if you want to leverage all of them
+            deploymentOptions.setWorkerPoolSize(instances);
+        }
+        deploymentOptions.setInstances(instances);
         logger.info(() -> "Deployment Options: " + deploymentOptions.toJson().encode());
     }
 }
