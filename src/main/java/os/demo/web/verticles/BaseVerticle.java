6a6fdd97f3ff310bc8f7151009a81e051535c543
oleksandr <oleksandr_sluchyk@epam.com>
2020-04-23 11:08:06 +0300

    fix cors

--- src/main/java/os/demo/web/verticles/BaseVerticle.java
+++ src/main/java/os/demo/web/verticles/BaseVerticle.java
@@ -1,15 +1,20 @@
 package os.demo.web.verticles;
 
+import io.netty.handler.codec.http.HttpHeaderNames;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServerOptions;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.CorsHandler;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import os.demo.web.config.AppConfig;
-import os.demo.web.handlers.CorsHandlerImpl;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static os.demo.web.verticles.Endpoints.ALL;
 
@@ -32,12 +37,13 @@ public abstract class BaseVerticle extends AbstractVerticle {
                         .setReusePort(config.getReusePort())
                         .setTcpFastOpen(true)
                         .setTcpNoDelay(true)
-                        .setTcpQuickAck(true);
+                        .setTcpQuickAck(true)
+                        .setAcceptBacklog(10_000);
 
                 Integer port = config.getPort();
 
                 Router router = Router.router(vertx);
-                router.route().handler(CorsHandlerImpl.configure());
+                enableCors(router);
                 BodyHandler bodyHandler = BodyHandler.create().setMergeFormAttributes(true);
                 router.put(ALL).handler(bodyHandler);
 
@@ -57,6 +63,31 @@ public abstract class BaseVerticle extends AbstractVerticle {
             }
         });
 
+    }
+
+    private void enableCors(Router router) {
+        Set<String> allowedHeaders = new HashSet<>();
+        allowedHeaders.add(HttpHeaderNames.ACCESS_CONTROL_ALLOW_METHODS.toString());
+        allowedHeaders.add(HttpHeaderNames.ACCESS_CONTROL_ALLOW_ORIGIN.toString());
+        allowedHeaders.add(HttpHeaderNames.ACCESS_CONTROL_ALLOW_CREDENTIALS.toString());
+        allowedHeaders.add(HttpHeaderNames.ORIGIN.toString());
+        allowedHeaders.add(HttpHeaderNames.CONTENT_TYPE.toString());
+        allowedHeaders.add(HttpHeaderNames.ACCEPT.toString());
+
+        Set<HttpMethod> allowedMethods = new HashSet<>();
+        allowedMethods.add(HttpMethod.GET);
+        allowedMethods.add(HttpMethod.POST);
+        allowedMethods.add(HttpMethod.DELETE);
+        allowedMethods.add(HttpMethod.PATCH);
+        allowedMethods.add(HttpMethod.OPTIONS);
+        allowedMethods.add(HttpMethod.PUT);
+
+        CorsHandler corsHandler = CorsHandler.create("*")
+                .allowCredentials(true)
+                .allowedHeaders(allowedHeaders)
+                .allowedMethods(allowedMethods);
+
+        router.route().handler(corsHandler);
     }
 
     protected abstract Router configureRouter(Vertx vertx, Router router);
